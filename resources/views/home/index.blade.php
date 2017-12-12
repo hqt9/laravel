d989a807e5fb6cb3d089d52cd3bfb90b864a7707
hqt9 <hqt_29@163.com>
2017-12-12 19:02:50 +0800

    change

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -54,58 +54,58 @@
         // $ = jQuery.noConflict();
         $(function(){
             $('.btn-login').on('click', function(){
-                var $this = $(this);
-                var email = $('#email').val();
-                var emailReg = /[\w-\.]+@([\w-]+\.)+[a-z]{2,3}/;
-                var password = $('#password').val();
-                var passwordReg = /^(\w){6,26}$/;
-                var isError = false;
-                var $form = $this.closest('form');
-                var url = $form.attr('action');
+            //     var $this = $(this);
+            //     var email = $('#email').val();
+            //     var emailReg = /[\w-\.]+@([\w-]+\.)+[a-z]{2,3}/;
+            //     var password = $('#password').val();
+            //     var passwordReg = /^(\w){6,26}$/;
+            //     var isError = false;
+            //     var $form = $this.closest('form');
+            //     var url = $form.attr('action');
 
-                if(email == '') {
-                    $('#email').addClass('error');
-                    $('#email ~ .msg-errors').text('This is a required field.').addClass('active');
-                    isError = true;
-                } else {
-                    if(!emailReg.test(email)) {
-                        $('#email').addClass('error');
-                        $('#email ~ .msg-errors').html('Please enter a valid email address:<br/>josh@some.com').addClass('active');
-                        isError = true;
-                    }
-                }
+            //     if(email == '') {
+            //         $('#email').addClass('error');
+            //         $('#email ~ .msg-errors').text('This is a required field.').addClass('active');
+            //         isError = true;
+            //     } else {
+            //         if(!emailReg.test(email)) {
+            //             $('#email').addClass('error');
+            //             $('#email ~ .msg-errors').html('Please enter a valid email address:<br/>josh@some.com').addClass('active');
+            //             isError = true;
+            //         }
+            //     }
 
-                if(password == '') {
-                    $('#password').addClass('error');
-                    $('#password ~ .msg-errors').text('This is a required field.').addClass('active');
-                    isError = true;
-                } else {
-                    if(!passwordReg.test(password)) {
-                        $('#password').addClass('error');
-                        $('#password ~ .msg-errors').html('Please enter a secure password:<br/>6-26 characters.').addClass('active');
-                        isError = true;
-                    }
-                }
+                // if(password == '') {
+                //     $('#password').addClass('error');
+                //     $('#password ~ .msg-errors').text('This is a required field.').addClass('active');
+                //     isError = true;
+                // } else {
+                //     if(!passwordReg.test(password)) {
+                //         $('#password').addClass('error');
+                //         $('#password ~ .msg-errors').html('Please enter a secure password:<br/>6-26 characters.').addClass('active');
+                //         isError = true;
+                //     }
+                // }
 
-                if(!isError) {
-                    //return true;
-                    $.ajax({
-                        url: url,
-                        data: $form.serialize(),
-                        beforeSend: function(){
-                            $('.login-loading').addClass('active');
-                        }
-                    }).done(function(response) {
-                        if (response.success !== false) {
-                            window.location.href = '/home';
-                        } else {
-                            $('.error').text('Error: Invalid login or password.' + response.message);
-                        }
-                        $('.login-loading').removeClass('active');
-                    });
-                }
-                return false;
-                // $('#form1').submit();
+                // if(!isError) {
+                //     //return true;
+                //     $.ajax({
+                //         url: url,
+                //         data: $form.serialize(),
+                //         beforeSend: function(){
+                //             $('.login-loading').addClass('active');
+                //         }
+                //     }).done(function(response) {
+                //         if (response.success !== false) {
+                //             // window.location.href = '/home';
+                //         } else {
+                //             $('.error').text('Error: Invalid login or password.' + response.message);
+                //         }
+                //         $('.login-loading').removeClass('active');
+                //     });
+                // }
+                // return false;
+                $('#form1').submit();
             });
 
             $('input').on('focus', function(){
